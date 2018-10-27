ce775f2bbaf51c78139fd7ecbbdd6a1476d580b2
kst123abc <kylesuero@gmail.com>
2018-10-27 11:36:17 -0400

    pushing the scraper

---
+++ homefacts.py
@@ -0,0 +1,23 @@
+from bs4 import BeautifulSoup
+import urllib3
+import json
+import re
+
+url ="https://www.homefacts.com/offenders.html"
+http=urllib3.PoolManager()
+response=http.request('GET', url)
+
+soup=BeautifulSoup(response.data)
+
+states=soup.find_all(href=re.compile("/offenders/"))
+for s in states:
+    http2 = urllib3.PoolManager()
+    response2=http2.request('GET', 'https://www.homefacts.com'+s.get('href'))
+    soup2=BeautifulSoup(response2.data)
+    county=soup2.find_all(href=re.compile('%s/' % s.get('href').strip('.html')))
+    for c in county:
+        http3=urllib3.PoolManager()
+        response3=http3.request('GET', 'https://www.homefacts.com'+c.get('href'))
+        soup3=BeautifulSoup(response3.data)
+        offenders=soup3.find_all(href=re.compile("https://homefactscom"+c.get('href')))
+        print(offenders)
