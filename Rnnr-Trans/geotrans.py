a7aea75a5b7084056a9206ee9e12f4d8acc34d48
dquach217 <dq6180@rit.edu>
2018-10-28 00:47:59 -0400

    geotrans

--- Rnnr-Trans/geotrans.py
+++ Rnnr-Trans/geotrans.py
@@ -1,12 +1,20 @@
+import ssl
+import certifi
 import sqlite3 as sql
+import geopy
 from geopy.geocoders import Nominatim
+
+ctx = ssl.create_default_context(cafile=certifi.where())
+geopy.geocoders.options.default_ssl_context=ctx
 
 table_name='offenders'
 conn=sql.connect('Rnnr.db.sqlite')
 con=conn.cursor()
-print(con.execute("SELECT * FROM offenders;"))
-geolocator = Nominatim(user_agent="specify_your_app_name_here")
-location = geolocator.geocode("175 5th Avenue NYC")
-print(location.address)
-print((location.latitude, location.longitude))
-print(location.raw)
+con.execute("SELECT * FROM offenders;")
+rows = con.fetchall()
+for r in rows:
+    geolocator = Nominatim(user_agent="Rnnr")
+    location = geolocator.geocode(r[2] + " " + r[3]+" "+r[4])
+    if location != None:
+        print((location.latitude, location.longitude))
+
