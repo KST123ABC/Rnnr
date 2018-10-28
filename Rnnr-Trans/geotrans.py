779501c8d793f18c36f501862c7e5ce0b838b777
kst123abc <kylesuero@gmail.com>
2018-10-28 01:46:09 -0400

    script

--- Rnnr-Trans/geotrans.py
+++ Rnnr-Trans/geotrans.py
@@ -15,7 +15,8 @@ con=conn.cursor()
 con.execute("SELECT * FROM offenders;")
 rows = con.fetchall()
 for r in rows:
-    if r[13] is None:
+    print(r[13])
+    if r[13] is "None":
         geolocator = Nominatim(user_agent="rnnr-app")
         location = geolocator.geocode(r[2] + " " + r[3]+" "+r[4])
         if location != None:
